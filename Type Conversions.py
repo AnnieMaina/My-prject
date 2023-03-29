c0e8341da462035e5cc834a626608ed073887c5b
AnnieMaina <93188790+AnnieMaina@users.noreply.github.com>
2023-03-29 12:21:43 +0300

    Add files via upload

---
+++ Type Conversions.py
@@ -0,0 +1,10 @@
+# Implicit conversion: The interpreter automatically converts one data type into another
+x = 20  # The integer is converted into a float
+y = 15.5
+print(x + y)
+
+# Explicit conversion:converting  one data type to another by calling the function for the data type we want to convert to
+base = 6
+height = 3
+area = (base *height) / 2
+print(f"The area of the triangle is = {area}")
